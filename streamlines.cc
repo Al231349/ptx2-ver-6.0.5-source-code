6cab072755b8563331e4991b2dba48101d11f347
Albert Liu <92066782+Al231349@users.noreply.github.com>
2023-03-11 22:13:24 -0600

    Add files via upload
    Different vector<float> initialization method

--- streamlines.cc
+++ streamlines.cc
@@ -1883,12 +1883,9 @@ namespace TRACT{
       if(connect){
         m_ConMat1->AddTo(m_curloc.loc+1,locs[j].first+1,locs[j].second.value);
         //3/10/23 addition - create int vector 
-        vector<float> current_vec;
-        current_vec[1] = m_curloc.loc+1;
-        current_vec[2] = locs[j].first+1;
-        current_vec[3] = locs[j].second.value;
+        vector<float> current_vect{ m_curloc.loc+1, locs[j].first+1, locs[j].second.value };
 
-        m_save_lengths.push_back(current_vec); 
+        m_save_lengths.push_back(current_vect); 
         
         if(opts.omeanpathlength.value()) m_ConMat1b->AddTo(m_curloc.loc+1,locs[j].first+1,1);
       }
